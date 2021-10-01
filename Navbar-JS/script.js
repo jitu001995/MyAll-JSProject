92c20b6d7e8a92337e787c766fcaf9914ea93c6e
jitu <k1995jitu@gmail.com>
2021-10-01 08:00:22 +0530

    Js Project

---
+++ Navbar-JS/script.js
@@ -0,0 +1,17 @@
+
+//classList-shows/get all classes
+//contains-checks classList for specific class
+//add-add class
+//remove-remove class
+//toggle-toggles class
+
+const navToggle=document.querySelectorAll(".nav-toggle");
+const links=document.querySelector(".links");
+
+navToggle.addEventListener('click',function(){
+     //console.log(links.classList);
+    // console.log(links.classList.contains('random'));
+   // console.log(links.classList.contains('links'));
+   
+  
+})
